82f9dac9be06ea77df40fd770f264aac1dd854eb
Trie0037 <trie0037@gmail.com>
2019-12-05 17:14:42 -0600

    sbuild

---
+++ build/precache-manifest.e2aca46b3ca9bc7a54715cf925fb0e45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e420242a43f01b82fdaaeb252d699ee4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43f85072395eb5f1e8bc",
+    "url": "/static/css/main.95ba7651.chunk.css"
+  },
+  {
+    "revision": "50c1c21d405f77dd67e4",
+    "url": "/static/js/2.48507412.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.48507412.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43f85072395eb5f1e8bc",
+    "url": "/static/js/main.ccfad7bb.chunk.js"
+  },
+  {
+    "revision": "b7c367a8728bfc283446",
+    "url": "/static/js/runtime-main.7438d2ef.js"
+  },
+  {
+    "revision": "e2b80abcace751c919b84de58dda709f",
+    "url": "/static/media/lightning.e2b80abc.jpg"
+  }
+]);
